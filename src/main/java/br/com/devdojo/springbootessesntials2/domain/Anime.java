6d445971accc7b1db00f244db1821361bd6c0c2c
michelspirlandeli <michelspirlandeli10@gmail.com>
2020-12-12 17:28:59 -0300

    first commit

---
+++ src/main/java/br/com/devdojo/springbootessesntials2/domain/Anime.java
@@ -0,0 +1,25 @@
+package br.com.devdojo.springbootessesntials2.domain;
+
+public class Anime {
+
+	private String name;
+
+	public Anime() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Anime(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ src/test/java/br/com/devdojo/springbootessesntials2/SpringBootEssesntials2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devdojo.springbootessesntials2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEssesntials2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
